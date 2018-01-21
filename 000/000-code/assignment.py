fbfb42258533b3491d1596f1d090b9d6fc6ad364
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-01-20 16:44:15 -0800

    Assignment 000 added

---
+++ 000/000-code/assignment.py
@@ -0,0 +1,56 @@
+print("Hello, world!")
+
+"""
+# I'm a single-line comment, so even when you remove the triple-quotes, I won't do anything
+
+print('When the triple quotes are there, I don\'t get executed either. But without them... ALSO PLEASE REMOVE ME K THX BAI')
+
+# TODO: implement
+def echo_me(msg):
+    pass
+
+# TODO: understand and remove
+def string_or_not(d):
+    exec(d)
+
+# TODO: understand formatting - can you eliminate the redundancy here?
+def append_msg(msg):
+    print("Your" + " message" + " was: {}.".format(msg))
+
+# TODO: understand class (an introduction)
+class QuickMaths():
+    def add(x, y):
+        return None
+
+    def subtract(x, y):
+        return None
+
+    def multiply(x, y):
+        return None
+
+    def divide(x, y):
+        return None
+
+# TODO: implement - can you do this more efficiently?
+def increment_by_one(lst):
+    new_lst = list()
+
+    for x in lst:
+        new_lst.append(x - 1)
+    
+    return new_lst
+
+# TODO: implement - do we need a return statement here? why?
+def update_name(person, new_name):
+    person["name"] = new_name
+
+    return person
+
+# TODO: implement - these are still required, but are combinations of learned skills + some
+def challenge1(lst):
+    pass
+
+# TODO: implement
+def challenge2():
+    pass
+"""
